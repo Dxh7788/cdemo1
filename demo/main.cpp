e43e6a465e4ef6847a6b6a7a247359a28ee69f5e
dongxiaohong <dongxiaohong@daokoudai.com>
2019-03-12 17:36:50 +0800

    Initial Commit

---
+++ demo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  demo
+//
+//  Created by 冻小宏 on 2019/3/12.
+//  Copyright © 2019年 冻小宏. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
